f8a6b364203ea74d0bc9f16566d7eea33bfec84f
Joel Pina <joel@brokenrubik.co>
2021-06-11 00:33:22 -0300

    Initial Commit

---
+++ src/app/heroes/pages/heroes-home/heroes-home.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-home',
+  templateUrl: './heroes-home.component.html',
+  styles: [ `
+  .container{
+    margin: 10px
+  }
+  `
+  ]
+})
+export class HeroesHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
